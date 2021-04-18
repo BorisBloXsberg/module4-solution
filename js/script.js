360e8889bf73a532158280cf13026f430a71be39
Mario Schirmer <an.den.mario@gmail.com>
2021-04-18 16:52:24 +0200

    Initial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("test");
+var names = ["John", "Luca", "Mario", "Alex"];
+
+for (var i = 0; i < names.length; i++) 
+{
+	var temp = names[i];
+	if (temp[0]== "J" || temp[0]== "j") 
+	{
+		console.log("Goodbye " + names[i]);
+	}
+	else
+	{
+		console.log("Hi " + names[i]);
+	}
+	
+}
